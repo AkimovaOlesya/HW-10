c7d2e4f5f0118e5ac4197902e73baf7df7e70e43
o.akimova <o.akimova@movavi.com>
2021-11-10 15:22:54 +0700

    Initial Commit

---
+++ Shared/HW_10App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_10App.swift
+//  Shared
+//
+//  Created by o.akimova on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
